16794560439b493c42bc21b16a55e30cdf50fee0
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-29 17:16:57 +0200

    MILESTONE 10 di SELECT.sql, risultato errato

--- SELECT.sql
+++ SELECT.sql
@@ -19,7 +19,7 @@
 ----4- Selezionare tutte le recensioni scritte dal giocatore con ID = 800 (11)
 --SELECT *
 --FROM reviews
---WHERE player_id = 800; --(risultato: 10)
+--WHERE player_id = 800; ----(risultato: 10)
 
 
 ----5- Contare quanti tornei ci sono stati nell'anno 2015 (9)
@@ -40,13 +40,24 @@
 --WHERE category_id = 2
 --OR category_id = 6;
 
---8- Selezionare tutte le recensioni con voto compreso tra 2 e 4 (2947)
-SELECT *
-FROM reviews
-WHERE rating >= 2
-AND rating <= 4; --(risultato: 2499)
---9- Selezionare tutti i dati dei videogiochi rilasciati nell'anno 2020 (46)
---10- Selezionare gli id dei videogame che hanno ricevuto almeno una recensione da stelle, mostrandoli una sola volta (443)
+
+----8- Selezionare tutte le recensioni con voto compreso tra 2 e 4 (2947)
+--SELECT *
+--FROM reviews
+--WHERE rating >= 2
+--AND rating <= 4; ----(risultato: 2499)
+
+
+----9- Selezionare tutti i dati dei videogiochi rilasciati nell'anno 2020 (46)
+--SELECT *
+--FROM videogames
+--WHERE YEAR(release_date) = 2020;
+
+
+----10- Selezionare gli id dei videogame che hanno ricevuto almeno una recensione da stelle, mostrandoli una sola volta (443)
+--SELECT DISTINCT videogame_id
+--FROM reviews
+--WHERE rating IS NOT NULL;  ----(risultato: 500)
 --*********** BONUS ***********
 --11- Selezionare il numero e la media delle recensioni per il videogioco con ID = 412 (review number = 12, avg_rating = 3)
 --12- Selezionare il numero di videogame che la software house con ID = 1 ha rilasciato nel 2018 (13)
