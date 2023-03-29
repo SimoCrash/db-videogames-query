f92a6f9f63885d77e4f7155ab9642f9b9f403a47
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-29 17:02:40 +0200

    MILESTONE 8 di SELECT.sql, risultato sbagliato

--- SELECT.sql
+++ SELECT.sql
@@ -13,7 +13,7 @@
 ----3- Selezionare tutti i giocatori il cui nome finisce per "a" (220)
 --SELECT *
 --FROM players
---WHERE name LIKE '%a';                                 -- qui LIKE ci permette di identificare una parte di stringa
+--WHERE name LIKE '%a';                                 ---- qui LIKE ci permette di identificare una parte di stringa
 
 
 ----4- Selezionare tutte le recensioni scritte dal giocatore con ID = 800 (11)
@@ -22,20 +22,29 @@
 --WHERE player_id = 800; --(risultato: 10)
 
 
---5- Contare quanti tornei ci sono stati nell'anno 2015 (9)
+----5- Contare quanti tornei ci sono stati nell'anno 2015 (9)
 --SELECT *
 --FROM tournaments
 --WHERE year = 2015;
 
 
---6- Selezionare tutti i premi che contengono nella descrizione la parola 'facere' (2)
+----6- Selezionare tutti i premi che contengono nella descrizione la parola 'facere' (2)
 --SELECT *
 --FROM awards 
 --WHERE description LIKE '%facere%';
 
 
---7- Selezionare tutti i videogame che hanno la categoria 2 (FPS) o 6 (RPG), mostrandoli una sola volta (del videogioco vogliamo solo l'ID) (287)
+----7- Selezionare tutti i videogame che hanno la categoria 2 (FPS) o 6 (RPG), mostrandoli una sola volta (del videogioco vogliamo solo l'ID) (287)
+--SELECT DISTINCT videogame_id							---- DISTINCT si usa per mostrare una sola volta
+--FROM category_videogame
+--WHERE category_id = 2
+--OR category_id = 6;
+
 --8- Selezionare tutte le recensioni con voto compreso tra 2 e 4 (2947)
+SELECT *
+FROM reviews
+WHERE rating >= 2
+AND rating <= 4; --(risultato: 2499)
 --9- Selezionare tutti i dati dei videogiochi rilasciati nell'anno 2020 (46)
 --10- Selezionare gli id dei videogame che hanno ricevuto almeno una recensione da stelle, mostrandoli una sola volta (443)
 --*********** BONUS ***********
